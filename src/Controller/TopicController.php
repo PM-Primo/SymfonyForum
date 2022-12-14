0217855e3086713d60211fedf14023824ad94c80
PM-Primo <pierrem.primo@gmail.com>
2022-12-14 11:51:37 +0100

    added display of Posts in a Topic

--- src/Controller/TopicController.php
+++ src/Controller/TopicController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Topic;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TopicController extends AbstractController
 {
@@ -15,6 +16,16 @@ class TopicController extends AbstractController
     {
         return $this->render('topic/index.html.twig', [
             'controller_name' => 'TopicController',
+        ]);
+    }
+
+    /**
+     * @Route("/topic/{id}", name="show_topic")
+     */
+    public function show(Topic $topic): Response
+    {
+        return $this->render('topic/show.html.twig', [
+            'topic' => $topic,
         ]);
     }
 }
