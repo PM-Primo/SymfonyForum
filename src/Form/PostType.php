9d263858de43ea41f3dd6887f49952355b328508
PM-Primo <pierrem.primo@gmail.com>
2022-12-15 16:06:11 +0100

    enlarged TextareaTypes

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -8,14 +8,15 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('textePost' , TextType::class, ["attr" => ["class" => "form-control"]])
-            ->add('submit', SubmitType::class, ["attr" => ["class" => "btn btn-success"]])
+            ->add('textePost' , TextareaType ::class, ["attr" => ["class" => "form-control", 'rows' => '4']])
+            ->add('submit', SubmitType::class, ["attr" => ["class" => "btn btn-success btn-submit"] ])
         ;
     }
 
--- src/Form/TopicType.php
+++ src/Form/TopicType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class TopicType extends AbstractType
 {
@@ -15,8 +16,8 @@ class TopicType extends AbstractType
     {
         $builder
             ->add('titreTopic' , TextType::class, ["attr" => ["class" => "form-control"]])
-            ->add('texteFirstPost' , TextType::class, ["attr" => ["class" => "form-control"], 'mapped' => false])
-            ->add('submit', SubmitType::class, ["attr" => ["class" => "btn btn-success"]])
+            ->add('texteFirstPost' , TextareaType::class, ["attr" => ["class" => "form-control", 'rows' => '5'], 'mapped' => false])
+            ->add('submit', SubmitType::class, ["attr" => ["class" => "btn btn-success btn-submit"]])
         ;
     }
 
